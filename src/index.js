4570b147efdeb57a9302dd2c7c264303ec27ab21
Vladimír Zdražil <mail@vzdrazil.com>
2018-12-03 22:47:04 +0100

    wip boxes id

--- src/index.js
+++ src/index.js
@@ -7,3 +7,4 @@ console.log(frequency.calibrate(lib.getInput("01")));
 console.log(frequency.findDuplicate(lib.getInput("01")));
 
 console.log(inventory.getChecksum(lib.getInput("02")));
+console.log(inventory.getDifferingCharacters(lib.getInput("02")));

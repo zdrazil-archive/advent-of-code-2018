42e4410e5668944020f236e09b3f50965d50317f
Vladimír Zdražil <mail@vzdrazil.com>
2018-12-03 22:47:04 +0100

    WIP: Boxes id

--- src/02.js
+++ src/02.js
@@ -1,13 +1,13 @@
 const R = require("ramda");
 
-exports.getFrequency = function(str) {
+exports.getFrequency = function getFrequency(str) {
   return str.split("").reduce((prev, curr) => {
     const newValue = prev[curr] ? prev[curr] + 1 : 1;
     return { ...prev, [curr]: newValue };
   }, {});
 };
 
-exports.getChecksum = function(boxIds) {
+exports.getChecksum = function getChecksum(boxIds) {
   const frequencies = boxIds.map(item => {
     const itemFrequencies = R.values(this.getFrequency(item));
     return [itemFrequencies.includes(2), itemFrequencies.includes(3)];
@@ -19,11 +19,45 @@ exports.getChecksum = function(boxIds) {
   return temp[0] * temp[1];
 };
 
-exports.getDifferingCharacters = function(boxIds) {
-  const result = boxIds.map(item => {
-    console.log(item);
-    return item.split("").sort((a, b) => a.localeCompare(b));
-  });
+exports.getDifferingCharacters = function getDifferingCharacters(boxIds) {
+  const result = boxIds.map((item, index) =>
+    boxIds.map((innerItem, innerIndex) => {
+      if (innerIndex === index) {
+        return [];
+      }
+      return R.symmetricDifference(item, innerItem);
+    })
+  );
   console.log(result);
-  return result;
+  const result2 = result.map(item =>
+    item.reduce((accum, innerItem, index) => {
+      if (innerItem.length === 2) {
+        return [...accum, index];
+      }
+      return accum;
+    }, [])
+  );
+  console.log(result2);
+
+  const result3 = R.uniq(
+    result2
+      .map((item, index) => {
+        // console.log(item.length);
+        const abc = "sdf";
+        if (boxIds[index] === undefined || boxIds[item] === undefined) {
+          return null;
+        }
+        return R.pair(boxIds[index], boxIds[item]);
+      })
+      .filter(item => item !== null)
+      .map(item => item.sort())
+  );
+  console.log(result3);
+  // return result3;
+  const result4 = result3
+    .map(item => R.intersection(item[0], item[1]))
+    .map(innerItem => innerItem.join(""));
+
+  console.log(result4);
+  return result4;
 };

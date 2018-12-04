e7367bccf2591c594032ea5dd40d9c81dc6c8ba6
Vladimír Zdražil <mail@vzdrazil.com>
2018-12-04 17:42:45 +0100

    Add nicer console.logs

--- src/index.js
+++ src/index.js
@@ -2,19 +2,12 @@ const lib = require("./lib");
 const frequency = require("./01");
 const inventory = require("./02");
 
-console.log(frequency.calibrate(lib.getInput("01")));
+console.log(`Calibrated frequency: ${frequency.calibrate(lib.getInput("01"))}`);
 
-console.log(frequency.findDuplicate(lib.getInput("01")));
+console.log(`Duplicate: ${frequency.findDuplicate(lib.getInput("01"))}`);
 
-console.log(inventory.getChecksum(lib.getInput("02")));
-const differentBoxIds = [
-  "abcde",
-  "fghij",
-  "klmno",
-  "pqrst",
-  "fguij",
-  "axcye",
-  "wvxyz"
-];
-// console.log(inventory.getCommonLetters(differentBoxIds));
-console.log(inventory.getCommonLetters(lib.getInput("02")));
+console.log(`Checksum: ${inventory.getChecksum(lib.getInput("02"))}`);
+
+console.log(
+  `Common letters: ${inventory.getCommonLetters(lib.getInput("02"))}`
+);

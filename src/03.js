11d4f3bb0c600607e2bedd626718b0554ef4924a
Vladimír Zdražil <mail@vzdrazil.com>
2018-12-06 16:02:31 +0100

    Add 03 tests

---
+++ src/03.js
@@ -0,0 +1,13 @@
+function getPositions(startPosition, multiples) {
+  const left = startPosition[0];
+  const top = startPosition[1];
+  const positions = multiples.map(
+}
+
+exports.parseClaims = function parseClaims(claims) {
+  return claims;
+};
+
+exports.getMultipleClaims = function getMultipleClaims(claims) {
+  return claims;
+};

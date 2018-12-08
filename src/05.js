2cea009ecb7bc0c214e112bf5d9229d5f63c3659
Vladimír Zdražil <mail@vzdrazil.com>
2018-12-08 09:38:08 +0100

    05 Rename function

--- src/05.js
+++ src/05.js
@@ -8,59 +8,21 @@ function isSameCase(char1, char2) {
   return false;
 }
 
-function containsSameCase(string) {
-  const result = string.reduce((prev, current) => {
-    if (prev === true) {
-      return true;
-    }
-    if (prev === "") {
-      return current;
-    }
-    if (isSameCase(prev, current)) {
-      return true;
-    }
-    return current;
-  }, "");
-
-  return result === true;
-}
-
-function removeUtil(string, sumString = []) {
-  // console.log(string);
+function removeReactions(string, sumString = []) {
   if (string.length === 0) {
     return sumString;
   }
   if (sumString.length >= 1 && isSameCase(R.last(sumString), R.head(string))) {
-    return () => removeUtil(R.tail(string), R.dropLast(1, sumString));
+    return () => removeReactions(R.tail(string), R.dropLast(1, sumString));
   }
 
   if (string.length === 2 && isSameCase(string[0], string[1])) {
-    return () => removeUtil(R.tail(R.tail(string)), sumString);
+    return () => removeReactions(R.tail(R.tail(string)), sumString);
   }
-  return () => removeUtil(R.tail(string), [...sumString, string[0]]);
+
+  return () => removeReactions(R.tail(string), [...sumString, string[0]]);
 }
 
-exports.remove = function remove(string) {
-  console.time("someFunction");
-  const newString = trampoline(removeUtil)(toList(string));
-
-  console.timeEnd("someFunction");
-  return toString(newString);
-};
-
-function toList(string) {
-  return string.split("");
-}
-
-function toString(x) {
-  return x.join("");
-}
-
-exports.reactPair = function reactPair(pairs) {
-  const abc = "";
-};
-
-exports.react = function react(polymer) {
-  const abc = "";
-  return this.remove(polymer);
+exports.react = function remove(string) {
+  return trampoline(removeReactions)(string.split("")).join("");
 };

8bd3c1d29336cc5792185d5fa0a15a2832cb9d81
Vladimír Zdražil <mail@vzdrazil.com>
2018-12-09 05:43:11 +0100

    Add better time function name

--- src/index.js
+++ src/index.js
@@ -17,10 +17,10 @@ console.log(
   `Polymer after reaction: ${polymer.react(lib.getInput("05")[0]).length}`
 );
 
-console.time("someFunction");
+console.time("findShortest");
 console.log(
   `Shortest polymer's length: ${
     polymer.findShortest(lib.getInput("05")[0]).length
   }`
 );
-console.timeEnd("someFunction");
+console.timeEnd("findShortest");

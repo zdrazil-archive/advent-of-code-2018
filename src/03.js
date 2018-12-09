0e4dfee2bf794ed652912c2a3f9adeaa994ccd4b
Vladimír Zdražil <mail@vzdrazil.com>
2018-12-09 15:16:02 +0100

    WIP: 03

--- src/03.js
+++ src/03.js
@@ -1,8 +1,57 @@
-function getPositions(startPosition, multiples) {
+const R = require("ramda");
+
+exports.getPositions = function getPositions(startPosition, size) {
   const left = startPosition[0];
   const top = startPosition[1];
-  const positions = multiples.map(
-}
+  const emptyRowArray = new Array(size[0]).fill(null);
+  const emptyColArray = new Array(size[1]).fill(null);
+  // console.log(emptyRowArray);
+  const columnPositions = emptyColArray
+    .map((item, index) => {
+      // console.log(item);
+      const cde = emptyRowArray.map((innerItem, innerIndex) => {
+        const b = startPosition[0] + innerIndex;
+        const c = startPosition[1] + index;
+        // console.log([b, c]);
+        // console.log(index);
+        return [b, c];
+      }); // console.log(cde);
+      return cde;
+    })
+    .reduce((acc, item) => [...acc, ...item], []);
+
+  // console.log(columnPositions);
+  return columnPositions;
+};
+
+exports.parseStartPositions = function parseStartPositions(claim) {
+  return claim
+    .split(" ")[2]
+    .replace(":", "")
+    .split(",")
+    .map(a => parseInt(a, 10));
+};
+exports.parseRectangleSize = function parseRectangleSize(claim) {
+  return claim
+    .split(" ")[3]
+    .split("x")
+    .map(a => parseInt(a, 10));
+};
+
+exports.getDuplicateSize = function getDuplicateSize(claims) {
+  const a = claims
+    .map(item => {
+      const position = this.parseStartPositions(item);
+      const size = this.parseRectangleSize(item);
+      const b = this.getPositions(position, size);
+      // console.log(b);
+      return b;
+    })
+    .reduce((acc, curr) => [...acc, ...curr], []);
+  const b = R.uniq(a);
+  const c = a.length - b.length;
+  return c;
+};
 
 exports.parseClaims = function parseClaims(claims) {
   return claims;
--- src/03.test.js
+++ src/03.test.js
@@ -1,1 +1,28 @@
+const fabric = require("./03");
+
 const claims = ["#1 @ 1,3: 4x4", "#2 @ 3,1: 4x4", "#3 @ 5,5: 2x2"];
+
+const claim = "#1 @ 1,3: 4x3";
+
+test("parses start positions", () => {
+  expect(fabric.parseStartPositions(claim)).toEqual([1, 3]);
+});
+
+test("parses rectangle sizes", () => {
+  expect(fabric.parseRectangleSize(claim)).toEqual([4, 3]);
+});
+
+test("gets taken positions", () => {
+  expect(fabric.getPositions([3, 2], [3, 2])).toEqual([
+    [3, 2],
+    [4, 2],
+    [5, 2],
+    [3, 3],
+    [4, 3],
+    [5, 3]
+  ]);
+});
+
+test("calculates number of duplicate positions", () => {
+  expect(fabric.getDuplicateSize(claims)).toBe(4);
+});

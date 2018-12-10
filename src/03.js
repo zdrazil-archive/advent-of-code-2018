20c90d3870d04a75e8eebaa57bcf96008170c759
Vladimír Zdražil <mail@vzdrazil.com>
2018-12-10 15:46:37 +0100

    WIP 03 fix removal

--- src/03.js
+++ src/03.js
@@ -25,7 +25,6 @@ exports.sortEvents = e => e.sort();
 const query = actives => {
   let ans = 0;
   let cur = -1;
-  // console.log(actives);
   actives.forEach(([x1, x2]) => {
     cur = R.max(cur, x1);
     ans += R.max(0, x2 - cur);
@@ -40,62 +39,37 @@ exports.getActive = function getActive(events) {
   const b = e.reduce(
     (acc, curr) => {
       const [y, type, x1, x2] = curr;
-      const { prevActive, ans, currY } = acc;
-      const newAns = ans + query(prevActive) * (y - currY);
-      let newActive = {};
+      const { prevActive, ans, curY } = acc;
+      const newAns = ans + query(prevActive) * (y - curY);
+      let newActive = [];
       if (type === OPEN) {
         newActive = [...prevActive, [x1, x2]].sort();
       } else {
-        const matchIndex = R.findIndex(a => a === [x1, x2])(prevActive);
+        const matchIndex = R.findIndex(R.equals([x1, x2]))(prevActive);
+        // console.log(prevActive);
+        // console.log([x1, x2]);
         // console.log(matchIndex);
-        newActive = R.remove(matchIndex, matchIndex, prevActive);
-        // console.log(newActive);
+        // console.log(matchIndex);
+        newActive =
+          matchIndex === -1 ? prevActive : R.remove(matchIndex, 1, prevActive);
+        console.log(newActive);
+        console.log("---");
       }
-      console.log(acc);
+      // console.log(newActive);
       return {
         prevActive: newActive,
         ans: newAns,
-        currY: y
+        curY: y
       };
     },
     {
       prevActive: [],
       ans: 0,
-      currY: e[0][0]
+      curY: e[0][0]
     }
   );
-  console.log(b.ans);
-  return b.ans % (10 ** 9 + 7);
-
-  // const rowsCount = R.last(e)[0];
-  // const emptyRows = new Array(rowsCount).fill(null);
-  // const filledRows = emptyRows.reduce((acc, row, index) => {
-  //   const a = e.filter => (item => index ===)
-  //     , [[]]})
-
-  // console.log(emptyRows);
-  // const a = e.reduce((acc, curr) => {
-  //   console.log(acc);
-  //   const [y, type, x1, x2] = curr;
-  //   if (type === OPEN) {
-  //     if (acc.length === 0) {
-  //       return [[curr]];
-  //     }
-  //     if (R.last(acc)[0] === curr[0]) {
-  //       return [R.dropLast(1, acc), [...R.last(acc), curr]];
-  //     }
-  //     return [...acc, curr];
-  //   }
-  //   const matchIndex = R.findIndex(b => b[3] === curr[3] && b[4] === curr[4])(
-  //     R.last(acc)
-  //   );
-  //   // console.log(matchIndex);
-  //   // console.log(curr);
-  //   console.log(R.last(acc));
-  //   const newYRow = R.remove(matchIndex, matchIndex, R.last(acc));
-  //   return acc;
-  //   return [...R.dropLast(1, acc), newYRow];
-  // }, []);
+  // console.log(b.ans);
+  return b.ans;
 };
 
 exports.parseStartPositions = function parseStartPositions(claim) {
